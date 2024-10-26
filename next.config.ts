08e2d3d7c9f1e074ae84f36376acea21981facc1
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2024-10-26 08:45:43 -0300

    feat: configura ambiente 🚀🚀🚀

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
 };
 
 export default nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import { chakraSystem } from "@/styles/chakra-theme";
 import "@/styles/globals.css";
+import StyledComponentsRegistry from "@/styles/styled-components.registry";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <StyledComponentsRegistry>
+      <ChakraProvider value={chakraSystem}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </StyledComponentsRegistry>
+  );
 }
