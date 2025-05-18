198fa4f4f5f8ef39b002386df51b58e52d3c99b6
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2025-05-18 18:40:14 -0300

    feat: adiciona ícone do Tiktok e atualiza o componente SocialLink para incluir título nos links

--- src/components/Layout/Footer/SocialMedia.tsx
+++ src/components/Layout/Footer/SocialMedia.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { FaInstagram } from 'react-icons/fa'
-import { FaFacebook } from 'react-icons/fa'
+import { FaInstagram, FaFacebook, FaTiktok } from 'react-icons/fa'
 
-type Props = React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & { children: React.ReactNode }
-const SocialLink: React.FC<Props> = ({ children, ...props }) => {
+type Props = React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & { children: React.ReactNode; title: string }
+const SocialLink: React.FC<Props> = ({ children, title, ...props }) => {
   return (
-    <a className="text-white sm:text-gray-300 sm:hover:text-white" target="_blank" {...props}>
+    <a className="text-white sm:text-gray-300 sm:hover:text-white" target="_blank" {...props} title={title}>
       {children}
     </a>
   )
@@ -14,11 +13,14 @@ const SocialLink: React.FC<Props> = ({ children, ...props }) => {
 export const SocialMedia: React.FC = () => {
   return (
     <div className="text-3xl/8 h-auto flex items-center justify-start gap-2 flex-wrap">
-      <SocialLink href="https://www.instagram.com/jrbordados">
-        <FaInstagram />
+      <SocialLink href="https://www.instagram.com/jrbordados" title="Instagram">
+        <FaInstagram title="Instagram" />
       </SocialLink>
-      <SocialLink href="https://www.facebook.com/jrbordados">
-        <FaFacebook />
+      <SocialLink href="https://www.facebook.com/jrbordados" title="Facebook">
+        <FaFacebook title="Facebook" />
+      </SocialLink>
+      <SocialLink href="FaTiktok" title="Tiktok">
+        <FaTiktok title="Tiktok" />
       </SocialLink>
     </div>
   )
