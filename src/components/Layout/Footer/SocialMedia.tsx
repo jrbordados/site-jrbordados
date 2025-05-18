1c16509d024a8d5055f5302a0cdc0794886dadb9
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2025-05-18 18:45:46 -0300

    feat: remove prop 'title' do componente SocialLink e adiciona 'aria-label' para acessibilidade

--- src/components/Layout/Footer/SocialMedia.tsx
+++ src/components/Layout/Footer/SocialMedia.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { FaInstagram, FaFacebook, FaTiktok } from 'react-icons/fa'
 
-type Props = React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & { children: React.ReactNode; title: string }
-const SocialLink: React.FC<Props> = ({ children, title, ...props }) => {
+type Props = React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & { children: React.ReactNode }
+const SocialLink: React.FC<Props> = ({ children, ...props }) => {
   return (
-    <a className="text-white sm:text-gray-300 sm:hover:text-white" target="_blank" {...props} title={title}>
+    <a className="text-white sm:text-gray-300 sm:hover:text-white" target="_blank" {...props}>
       {children}
     </a>
   )
@@ -13,13 +13,13 @@ const SocialLink: React.FC<Props> = ({ children, title, ...props }) => {
 export const SocialMedia: React.FC = () => {
   return (
     <div className="text-3xl/8 h-auto flex items-center justify-start gap-2 flex-wrap">
-      <SocialLink href="https://www.instagram.com/jrbordados" title="Instagram">
+      <SocialLink href="https://www.instagram.com/jrbordados" title="Instagram" aria-label="Ir para o instagram">
         <FaInstagram title="Instagram" />
       </SocialLink>
-      <SocialLink href="https://www.facebook.com/jrbordados" title="Facebook">
+      <SocialLink href="https://www.facebook.com/jrbordados" title="Facebook" aria-label="Ir para o facebook">
         <FaFacebook title="Facebook" />
       </SocialLink>
-      <SocialLink href="FaTiktok" title="Tiktok">
+      <SocialLink href="FaTiktok" title="Tiktok" aria-label="Ir para o tiktok">
         <FaTiktok title="Tiktok" />
       </SocialLink>
     </div>
